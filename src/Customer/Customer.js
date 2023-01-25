371d179d06517e8974e333fee30b22d24c816e3d
Debora Carvalho Leite Wessen <debora14leite@gmail.com>
2023-01-24 23:52:51 -0500

    first commit

---
+++ src/Customer/Customer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CustomerHeader from './CustomerHeader';
+// import CustomerMenu from './CustomerMenu';
+
+
+function Customer () {
+    return(
+        <>
+        <CustomerHeader/>
+        {/* <CustomerMenu/> */}
+        <p>dadadasd</p>
+        </>
+        
+    );
+}
+
+export default Customer;
---
+++ src/Customer/CustomerHeader.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import imgMkt from "../Images/Logo_MarketPlace.png"
+
+
+function CustomerHeader () {
+    return(
+        <div>
+             <p>teste adad</p>
+        </div>
+
+    );
+}
+
+export default CustomerHeader;
